365a079a1227f045e92f440e541194792dc836a2
yume <Bush20000@aliyun.com>
2015-03-02 04:27:52 +0800

    Delet extra code

--- cpsImgBrowser.py
+++ cpsImgBrowser.py
@@ -14,7 +14,8 @@ except:
 import os
 import rarfile
 import hashlib
-import threading, time
+import threading
+import time
 import zipfile
 
 BACK_IMG = 1
@@ -46,12 +47,6 @@ def getFileMD5(uri):
     md5file.close()
     return md5
 
-def getpagehtml(pageurl):
-    req2=urllib2.Request(pageurl)
-    _response2=urllib2.urlopen(req2)
-    _d2=_response2.read()
-    return _d2
-
 def resize(w, h, w_box, h_box, pil_image):
     f1 = 1.0*w_box/w
     f2 = 1.0*h_box/h
@@ -82,15 +77,6 @@ def ShowAjoke(root,label,imgInfo):
     global postion
     flieName = imgInfo.filename.split('\\')[-1].encode('utf-8')
     if (imgCache[postion] == ""):
-        #print "download %d"%postion
-        #print root.width
-        #image_bytes = urllib.urlopen(url).read()
-        ## internal data file
-        #data_stream = io.BytesIO(image_bytes)
-        # open as a PIL image object
-        #if (not os.path.exists(RAR_TMP_URI + imgInfo.filename.replace('\\','/'))):
-        #    CPS_FILE.extract(imgInfo, path=RAR_TMP_URI)
-        #pil_image = Image.open(RAR_TMP_URI + imgInfo.filename.replace('\\','/'))
         data = CPS_FILE.read(imgInfo)
         pil_image = Image.open(io.BytesIO(data))
         imgCache[postion] = pil_image
@@ -112,8 +98,6 @@ def ShowAjoke(root,label,imgInfo):
     sf = "图片浏览器-%d/%d- %d/%d (%dx%d) %s --%s "%(mFilePos, len(fileList), postion + 1, IMG_SUM, wr, hr, flieName, fileList[mFilePos])
     root.title(sf)
 
-    #title['text']=url
-    #title.pack(fill = "x",expand = 1)
     tk_img = PhotoImage(pil_image_resized)
     label.configure(image = tk_img)
     label.image= tk_img
@@ -130,7 +114,7 @@ def onKeyPress(ev):
     global slideT
     global SLIDE_START
     global mFilePos
-    print(ev.keycode)
+    #print(ev.keycode)
     if ( ev.keycode == 111 or ev.keycode == 113):
         ShowPic(BACK_IMG)
     elif(ev.keycode == 114 or ev.keycode == 116):
@@ -170,7 +154,7 @@ def onKeyPress(ev):
 
 def openFile(mFilePos):
     mFilePos %= len(fileList)
-    print(fileList[mFilePos])
+    #print(fileList[mFilePos])
     if(fileList[mFilePos].split('.')[-1].lower() == 'rar'):
         return openRarFile(mFilePos)
     elif(fileList[mFilePos].split('.')[-1].lower() == 'zip'):
@@ -188,9 +172,9 @@ def openZipFile(mFilePos):
     if not os.path.exists(FILE_URI + fileList[mFilePos]):
         print("error:fileURI not exists")
         exit()
-    StartTime = time.time()
+    #StartTime = time.time()
     FILE_MD5 = getFileMD5(FILE_URI + fileList[mFilePos])
-    print(time.time() - StartTime)
+    #print(time.time() - StartTime)
 
     try:
         with open('./Pwd.json','r') as f:
@@ -235,7 +219,7 @@ def openZipFile(mFilePos):
                         PWD_JSON.update({FILE_MD5: p})
                         pwdJson = json.dumps(PWD_JSON, ensure_ascii=False)
                         with open('./Pwd.json', 'w') as f:
-                            f.write(unicode(pwdJson).encode('utf-8'))
+                            f.write(pwdJson.encode('utf-8'))
                         break
                     except:
                         pass
@@ -251,7 +235,7 @@ def openZipFile(mFilePos):
                     PWD_JSON.update({FILE_MD5: PWD})
                     pwdJson = json.dumps(PWD_JSON,ensure_ascii=False)
                     with open('./Pwd.json', 'w') as f:
-                        f.write(unicode(pwdJson).encode('utf-8'))
+                        f.write(pwdJson.encode('utf-8'))
                 except:
                     print("Password is WRONG !")
     list=getImageList()
@@ -272,12 +256,9 @@ def openRarFile(mFilePos):
     if not os.path.exists(FILE_URI + fileList[mFilePos]):
         print("error:fileURI not exists")
         exit()
-    StartTime = time.time()
+    #StartTime = time.time()
     FILE_MD5 = getFileMD5(FILE_URI + fileList[mFilePos])
-    print(time.time() - StartTime)
-    #RAR_TMP_URI = TEMP_DIR + FILE_MD5 + '/'
-    #if not os.path.exists(RAR_TMP_URI):
-    #    os.mkdir(RAR_TMP_URI)
+    #print(time.time() - StartTime)
 
     try:
         with open('./Pwd.json','r') as f:
@@ -318,7 +299,7 @@ def openRarFile(mFilePos):
                         PWD_JSON.update({FILE_MD5:p})
                         pwdJson = json.dumps(PWD_JSON,ensure_ascii=False)
                         with open('./Pwd.json','w') as f:
-                            f.write(unicode(pwdJson).encode('utf-8'))
+                            f.write(pwdJson.encode('utf-8'))
                         break
                     except:
                         pass
@@ -334,7 +315,7 @@ def openRarFile(mFilePos):
                     PWD_JSON.update({FILE_MD5:PWD})
                     pwdJson = json.dumps(PWD_JSON,ensure_ascii=False)
                     with open('./Pwd.json','w') as f:
-                        f.write(unicode(pwdJson).encode('utf-8'))
+                        f.write(pwdJson.encode('utf-8'))
                 except:
                     print("Password is WRONG !")
         #try:
@@ -353,15 +334,14 @@ def openRarFile(mFilePos):
 
 '''入口'''
 if __name__ == '__main__':
-    FILE_URI = './tkp.rar'
-    FILE_URI = "/media/bush/Download/IDM Downloads/Compressed/"
+    FILE_URI = input("Please input uri: ")
+    if (FILE_URI == ""):
+        FILE_URI = "/media/bush/Download/IDM Downloads/Compressed/"
 
     fileList = os.listdir(FILE_URI)
     fileList = [f for f in fileList if (f.split('.')[-1].lower() == 'rar' or f.split('.')[-1].lower() == 'zip')]
     mFilePos = 0
-    #TEMP_DIR = '/tmp/myRarTemp/'
-    #if not os.path.exists(TEMP_DIR):
-    #    os.mkdir(TEMP_DIR)
+
     slideT = threading.Timer(0, slide)
     Lock = threading.Lock()
     SLIDE_START = False
@@ -377,21 +357,7 @@ if __name__ == '__main__':
     while(not openFile(mFilePos)):
         del fileList[mFilePos]
 
-    ''' 标题定义'''
-    #title= tk.Label(root, text="",fg='blue' , bg='gray',font='Helvetica -18 bold')
-    #title.pack(fill = "x",expand = 1)
-    ''' 翻页按钮定义'''
-    #button = tk.Button(root, text='前一个', command= lambda :ShowPic(1),activeforeground='white',activebackground='red')
-    #button.pack(side = "left",padx=20)
-    #button = tk.Button(root, text='后一个', command= lambda :ShowPic(2),activeforeground='white',activebackground='red')
-    #button.pack(side = "right",padx=20)
-
-    #scale = tk.Scale(root, from_ = 0.1, to = 10, orient = tk.HORIZONTAL, resolution = 0.1, command = resizePic, width = 30)
-    #scale.set(1)
-    #scale.pack(fill="x", side = "bottom")
-    ''' 图片框定义'''
     label = tk.Label(root, image="", width=w_box, height=h_box)
     label.pack(padx=15, pady=15, expand = 1, fill = "both")
-    #ShowAjoke(root,title,label,list[postion])
     ShowAjoke(root,label,list[postion])
     root.mainloop()
